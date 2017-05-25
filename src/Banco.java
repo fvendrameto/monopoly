19d61715a6f9e1f555150c101119c704f7c9d2b9
suttergustavo <suttergustavo@live.com>
2017-05-25 14:18:58 -0300

    Funcionalidades básicas do banco

---
+++ src/Banco.java
@@ -0,0 +1,22 @@
+
+public class Banco {
+	public static void comprarPropriedade(Propriedade propriedade, Jogador jogador){
+		if(propriedade.getJogador() == null){ //caso a propriedade não tenha dono
+			if(jogador.getSaldo() >= propriedade.getPreco()){ //caso o jogador tenha dinheiro
+				jogador.adicionarPropriedade(propriedade);
+				jogador.sacarDinheiro(propriedade.getPreco());
+				propriedade.setDono(jogador);
+			}else{
+				System.out.println("Saldo insuficiente para fazer a compra");
+			}
+		}
+	}
+	
+	public static void hipotecaPropriedade(Propriedade propriedade, Jogador jogador){
+		if(propriedade.getJogador() == jogador){ //caso o jogador seja o dono da propriedade
+			propriedade.setDono(null); //propriedade fica sem nome
+			jogador.removerPropriedade(propriedade); 
+			jogador.sacarDinheiro(propriedade.getValorHipoteca());
+		}
+	}
+}
