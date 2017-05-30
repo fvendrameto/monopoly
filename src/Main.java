da438f161b16667f3a7d680cd7ff4f5a2f4812e8
fvendrameto <fernandovendrameto@gmail.com>
2017-05-30 16:31:27 -0300

    Classes tabuleiro e dados refeitas

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Main {
 	ArrayList<Jogador> jogadores;
-	Tabuleiro tabuleiro; 
+	Tabuleiro tabuleiro = new Tabuleiro();
 
 	//SÓ UMA IDEIA DE IMPLEMENTAÇÃO MUITO SIMPLES, AINDA É PRECISO PENSAR NO SISTEMA DE SERVIDOR E CLIENTE
 	public static void main(){
@@ -10,14 +10,14 @@ public class Main {
 		Jogador jogador_atual;
 		int dados;
 		
-		initComponentes();	
+		initComponentes();
 		while(tabuleiro.jogoContinua()){
 			idx_jogador_atual = tabuleiro.getJogadorAtual();
 			jogador_atual = jogadores[jogador_atual];
 			
-			dados = Dados.jogarDados();
-			posicao = jogador_atual.andarPosicaoTabuleiro(dados);
-			espaco_atual = tabuleiro.getEspacoPosicao(posicao);
+			dados = Dados.rolar(2, new int[2]);
+			int posicao = jogador_atual.andarPosicaoTabuleiro(dados);
+			Espaco espaco_atual = tabuleiro.getEspacoPosicao(posicao);
 			
 			if(espaco_atual instanceof Propriedade){
 				if(espaco_atual.getJogador() != null){
--- src/Tabuleiro.java
+++ src/Tabuleiro.java
@@ -1,18 +1,14 @@
 import java.util.ArrayList;
 
 public class Tabuleiro {
-	private ArrayList<Carta> cartas;
 	private ArrayList<Espaco> espacos;
 	private ArrayList<Jogador> jogadores;
-	private Dados dados = new Dados(2);
 	private int[] ordem;
 	private boolean[] temDinheiro;
-	private int jogadorAtual;
 	private int indJogador;
 	
-	public Tabuleiro(ArrayList<Jogadores> j, ArrayList<Carta> c, ArrayList<Espaco> e) {
+	public Tabuleiro(ArrayList<Jogador> j, ArrayList<Espaco> e) {
 		this.jogadores = j;
-		this.cartas = c;
 		this.espacos = e;
 		
 		this.ordem = new int[this.jogadores.size()];
@@ -23,36 +19,14 @@ public class Tabuleiro {
 		}
 	}
 	
-	public void sortearJogadores() {
-		int[] somaJogador = new int[this.jogadores.size()];
-		int i=0;
-		
-		for(Jogador j : this.jogadores) {
-			this.dados.rolar();
-			somaJogador[i++] = this.dados.getSomaValores();
-		}
-		
-		for(i=0; i<this.jogadores.size(); i++) {
-			ordem[i] = indiceValorMaximo(somaJogador);
-		}
-	}
-	
-	private int indiceValorMaximo(int[] vals) {
-		int max = Integer.MIN_VALUE, indMax = -1, i;
-		
-		for(i=0; i<vals.length; i++) {
-			if(vals[i] > max) {
-				max = vals[i];
-				indMax = i;
-			}
-		}
-		
-		vals[indMax] = -1;
-		
-		return indMax;
+	private void proximoIndJogador() {
+		do {
+			this.indJogador = ((this.indJogador+1) % this.jogadores.size());
+		} while(!this.temDinheiro[this.indJogador]);
 	}
 	
 	public int getJogadorAtual() {
+		proximoIndJogador();
 		return this.ordem[this.indJogador];
 	}
 	
@@ -66,15 +40,6 @@ public class Tabuleiro {
 		if(cont > 2)
 			return true;
 		return false;
-	}
-	
-	public void proximoJogador() {
-		do {
-			if(this.indJogador+1 >= this.jogadores.size())
-				this.indJogador = 0;
-			else
-				this.indJogador++;
-		} while(!this.temDinheiro[this.indJogador]);
 	}
 	
 	public Espaco getEspacoPosicao(int posicao) {
